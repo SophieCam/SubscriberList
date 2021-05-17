1477d2e8645bf1d80c96c68f9aa737e9d7ef53c7
SophieCam <79154836+SophieCam@users.noreply.github.com>
2021-05-17 17:47:15 -0400

    first commit

---
+++ src/main/java/com/tts/day5/repository/SubscriberRepository.java
@@ -0,0 +1,8 @@
+package com.tts.day5.repository;
+
+import com.tts.day5.model.Subscriber;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriberRepository extends CrudRepository<Subscriber, Long> {
+
+}
